4479ba5a787befde244d46d16a545d9e86402da4
ayomide <ayomidekareem563@gmail.com>
2024-07-07 22:00:04 -0700

    foot

--- task2/src/footer/footermobile.jsx
+++ task2/src/footer/footermobile.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "../footer/footermobile.css"
-import { ReactComponent as Heart } from '../img/heart.svg';
-import { ReactComponent as Home } from "../img/home.svg";
-import { ReactComponent as Menu } from "../img/menu.svg";
-import { ReactComponent as Cart } from "../img/cart-fill.svg";
+import { ReactComponent as Heart } from '../img/heart (2).svg';
+import { ReactComponent as Home } from "../img/house.svg";
+import { ReactComponent as Menu } from "../img/grid3.svg";
+import { ReactComponent as Cart } from "../img/cart (2).svg";
 import { Link } from "react-router-dom";
+// import home from "../img/home.png"
 function Footermobile(){
     return <div>
         <section className="footerMobileMain">
-            <Home className="home icon"/>
-            <Menu className="menu icon"/>
-            <Heart className="heart icon"/>
-            <Link to="/cart">
+            <a href="/body"><Home className="home icon"/></a>
+            <a href="/onboarding"><Menu className="menu icon"/></a>
+            <a><Heart className="heart icon"/></a>
+            <Link to="/cart" className="fli">
             <Cart className="footercheckout icon"/>
             </Link>
         </section>
